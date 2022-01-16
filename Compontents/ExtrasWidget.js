1557c8a8b563912586ecbc13f3581f07cd883d2a
halaxysounds <halaxysounds@gmail.com>
2022-01-16 15:13:42 -0300

    removed unnecessary imports

--- Compontents/ExtrasWidget.js
+++ Compontents/ExtrasWidget.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View, SafeAreaView, Image, TextInput } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 import { Ionicons } from '@expo/vector-icons';
 
