b420dd2d5b0753274aae1c833afd227bca5e00b2
halaxysounds <halaxysounds@gmail.com>
2022-01-21 13:00:50 -0300

    added animations, refactored code

--- Compontents/Screens/Search.js
+++ Compontents/Screens/Search.js
@@ -1,72 +1,48 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
-import { StyleSheet, View, TextInput, Text, ScrollView, TouchableHighlight } from 'react-native';
+import { Animated, StyleSheet, View, TextInput, Text, ScrollView, TouchableHighlight } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import Animations from '../Animations/Animations';
 
 import axios from 'axios';
 import Header from '../Header';
 
 function Search(props){
-
     const [cities, setCities] = useState([])
     const [input, setInput] = useState('')
-    let count = 0;
 
-    function getCities(query){
-        count += 1
-        console.log('searching for', query, count)
-        // new Promise(r => setTimeout(r, 500));
-        if (!query) return
-        const url = `http://geodb-free-service.wirefreethought.com/v1/geo/cities?limit=5&offset=0&namePrefix=${query}`
+    const anim = useRef(new Animated.Value(100)).current
 
-        axios.get(url).then(e => {
-            console.log(e.data.data[0].city + '\n')
-            setCities(e.data.data)
-        })
-    }
-
+    useEffect(() => {
+        Animations(anim, 0, 500)
+        return null
+    }, [])
+    
     const font = props.font
-
     const styles = StyleSheet.create({
         container: {
-            position: 'absolute',
+            transform: [{translateY: anim}],
             height: '100%',
-            width: '100%',
             backgroundColor: props.background,
-            top: '0%',
-            left: '0%',
             zIndex: 1,
-            // paddingTop: '15%',
-            // paddingHorizontal: '3%',
-            // borderRadius: 20,
-            // elevation: 10,
-            // margin: '3%'
         },
         searchBar: {
             backgroundColor: props.widgetColor,
-            // borderWidth: 1,
-            // borderColor: accent,
             borderRadius: 20,
             marginHorizontal: '3%',
-            // marginVertical: 5,
             height: '7%',
             alignItems: 'center',
             paddingHorizontal: '3%',
             flexDirection: 'row',
-            // elevation: 15,
-            // marginTop: '5%',
             marginBottom: '5%'
         },
         searchBarInput: {
             marginHorizontal: '3%',
-            width: '100%',
             fontFamily: font.regular,
             color: props.textColor
         },
         resultItem: {
             borderColor: props.widgetColor,
-            // height: '10%',
-            // width: '100%',
             borderRadius: 20,
             padding: '3%',
             marginHorizontal: '3%',
@@ -82,54 +58,69 @@ function Search(props){
         }
     })
 
+
+    //fetch cities by query
+    async function getCities(query){
+        if (!query) return
+        const url = `http://geodb-free-service.wirefreethought.com/v1/geo/cities?limit=5&offset=0&namePrefix=${query}`
+        console.log(`looking for ${query}`)
+
+        axios.get(url).then(e => {
+            setCities(e.data.data)
+        }).catch(err => console.log(err))
+    }
+
+
+
     return (
-        <View style={styles.container}>
+        <Animated.View style={styles.container}>
+
             <Header
-            
             isSearching = {props.isSearching}
             setIsSearching = {props.setIsSearching}
             setLocationData = {props.setLocationData}
             isSetting={props.isSetting}
             setIsSetting={props.setIsSetting}
+
             textColor = {props.textColor}
             font = {font}
             background = {props.background}
+
             title='Search'
             iconName='search'
-            
             />
+
             <View style={styles.searchBar}>
                 <Ionicons name="search" size={24} color={props.textColor} />
+                
                 <TextInput
                 style={styles.searchBarInput}
                 placeholder="Search for any city"
                 placeholderTextColor= {props.textColor}
                 onChangeText={e => {
-                    // waitCounter = 0;
-                    // updateResults(e)
                     setInput(e)
                     getCities(e)
                 }}
                 />
             </View>
-            <ScrollView>
-                {input == '' && <Text style={styles.noSearch}>Look any city up...</Text>}
 
-                
+            <ScrollView>
+
+                {input == '' && <Text style={styles.noSearch}>Look any city up...</Text>}
+                {/* map results */}
                 { input != '' && cities.map(city => {
                         return(
                             <TouchableHighlight
-                            style={styles.resultItem}
-                            onPress={() => {
-                                props.setLocationData({
-                                    latitude: city.latitude,
-                                    longitude: city.longitude
-                                })
-                                props.setIsSearching(false)
-                            }}
-                            activeOpacity={0.7}
-                            underlayColor="black"
-                            >
+                                style={styles.resultItem}
+                                onPress={() => {
+                                    props.setLocationData({
+                                        latitude: city.latitude,
+                                        longitude: city.longitude
+                                    })
+                                    props.setIsSearching(false)
+                                }}
+                                activeOpacity={0.7}
+                                underlayColor={props.background}>
                                 <View>
                                     <Text style={{color: props.textColor, fontSize: 18}} key={cities.indexOf(city)}>{city.city}, {city.regionCode}</Text>
                                     <Text style={{color: props.textColor, fontSize: 12}} key={cities.indexOf(city) + city.city}>{city.country}</Text>
@@ -139,8 +130,9 @@ function Search(props){
                     })
                 }
             </ScrollView>
-        </View>
+        </Animated.View>
     )
+
 }
 
 export default Search
