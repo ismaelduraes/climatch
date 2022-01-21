a04ddcd2c798b6cff4d5f01df0284b41f31c2bbd
halaxysounds <halaxysounds@gmail.com>
2022-01-21 12:59:08 -0300

    refactored code

--- Compontents/Header.js
+++ Compontents/Header.js
@@ -1,61 +1,70 @@
 // import { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Dimensions, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 import { Ionicons } from '@expo/vector-icons';
 
 
 function Header(props){
-
+    // console.log(props.setIsSearching)
     const font = props.font
-
     const styles = StyleSheet.create({
         header: {
-            backgroundColor: props.background,
-            // paddingBottom: '2%',
-            // flex: 0,
             justifyContent: 'space-between',
             alignItems: 'center',
             paddingHorizontal: '5%',
             paddingTop: '10%',
             paddingBottom: '5%',
-            // borderRadius: 15,
-            // elevation: 10,
-            // marginBottom: 0,
-            // marginTop: '15%',
             flexDirection: 'row',
-            zIndex: 1
-          },
-          headerTitle: {
+            zIndex: 1,
+            top: -1,
+            height: Math.round(Dimensions.get('window').height)*0.15,
+        },
+        headerTitle: {
             fontFamily: font.bold,
             fontSize: 30,
             color: props.textColor
-            // fontWeight: 'bold',
-          },
-
+        },
+        icon: {
+            marginLeft: '15%'
+        }
     })
 
     return (
-        <View style={styles.header}>
-            {/* screen title */}
-            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                {/* logo only renders on main screen */}
-                <Ionicons style={{marginRight: 10}} name={props.iconName} size={24} color={props.textColor}/>
+        <LinearGradient colors = {[props.background, props.background, 'transparent']} style = {styles.header}>
 
-                <Text style={styles.headerTitle}>{props.title}</Text>
+            <View style = {{flexDirection: 'row', alignItems: 'center'}}>
+                <Ionicons
+                    style = {{marginRight: 10}}
+                    name = {props.iconName}
+                    size = {24}
+                    color = {props.textColor}
+                />
 
+                <Text style = {styles.headerTitle}>
+                    {props.title}
+                </Text>
             </View>
-                
+
             {(!props.isSearching && !props.isSetting) &&
-                <View style={{flexDirection: 'row', width: '35%', justifyContent: 'space-between'}}>
-                    <Ionicons onPress={() => props.setLocationData(props.initialLocationData)} name="location-outline" size={26} color={props.textColor}/>
-                    <Ionicons onPress={() => props.setIsSearching(true)} name="search" size={26} color={props.textColor}/>
-                    <Ionicons onPress={() => props.setIsSetting(true)} name="settings-outline" size={26} color={props.textColor}/>
+
+                <View style = {{flexDirection: 'row', width: '35%', justifyContent: 'flex-end'}}>
+                    {props.locationData !== props.initialLocationData && <Ionicons onPress = {() => props.setLocationData(props.initialLocationData)}
+                        name="arrow-down" size = {26} color = {props.textColor}
+                        style={styles.icon}/>}
+                    <Ionicons onPress = {() => props.setIsSearching(true)}
+                        name="search" size = {26} color = {props.textColor}
+                        style={styles.icon}/>
+                    <Ionicons onPress = {() => props.setIsSetting(true)}
+                        name="settings-outline" size = {26} color = {props.textColor}
+                        style={styles.icon}/>
                 </View>
+
             }
-            {props.isSearching && <Ionicons onPress={() => props.setIsSearching(false)} name="close" size={26} color={props.textColor}/>}
-            {props.isSetting && <Ionicons onPress={() => props.setIsSetting(false)} name="close" size={26} color={props.textColor}/>}
-            {/* <Ionicons name="settings-outline" size={26} color={props.textColor}/> */}
-        </View>
+            {props.isSearching && <Ionicons onPress = {() => props.setIsSearching(false)} name="close" size = {26} color = {props.textColor}/>}
+            {props.isSetting && <Ionicons onPress = {() => props.setIsSetting(false)} name="close" size = {26} color = {props.textColor}/>}
+            {/* <Ionicons name="settings-outline" size = {26} color = {props.textColor}/> */}
+        </LinearGradient>
     )
 }
 
